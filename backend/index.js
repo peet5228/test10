b9f248efb8843d453837cfb467a957080db84432
peet5228 <ppeet5228@gmail.com>
2025-12-03 09:32:36 +0700

    test project

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const fileupload = require('express-fileupload')
+const cors = require('cors')
+const path = require('path')
+const app = express()
+
+app.use(cors ({
+    origin:'http://localhost:5173',
+    credentials:true
+}))
+
+app.use(express.json())
+app.use(fileupload())
+app.use('/uploads',express.statis('uploads'))
+
+
+// 404
+app.use((req,res) => res.status(404).json({message:'ปิดปรับปรุง!'}))
+
+app.listen(3001, () => console.log('Server Running On Port 3001'))
