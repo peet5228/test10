d9b1ef73ab25fa6161758b747b64107b54189d0f
peet5228 <ppeet55228@gmail.com>
2025-12-03 11:01:17 +0700

    com

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,9 +2,6 @@ const BASE_URL = import.meta.env?.VITE_BASE_API || 'http://localhost:3001/api'
 
 export const Staff = `${BASE_URL}/Staff`
 export const Eva = `${BASE_URL}/Eva`
-<<<<<<< HEAD
-export const Commit = `${BASE_URL}/Commit`
-=======
 export const Commit = `${BASE_URL}/Commit`
 export const api = `${BASE_URL}`
->>>>>>> 69ee7ba8ffb94c092f794799ce94ea51fb54584e
+
